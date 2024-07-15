20e340483ecf0b5f2b4dd71c6c5ccbba0558cfcf
Ethan Klausmeyer <112035330+Fullchord@users.noreply.github.com>
2024-07-14 21:09:49 -0500

    Fullchord Modding test

--- common/defines/BHVM.lua
+++ common/defines/BHVM.lua
@@ -1,28 +1,48 @@
+NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 999 -- Lag days
+NDefines.NGame.LAG_DAYS_FOR_PAUSE = 999 -- pause days
+NDefines.NGame.GAME_SPEED_SECONDS = { 6000.0, 0.25, 0.19, 0.06, 0.0 }
+NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 14
+NDefines.NGame.MESSAGE_TIMEOUT_DAYS = 14
 
-NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 50
 
-NDefines_CareerProfile.NCareerProfile.MOD_STATISTICS_GROUP = "BHVM"
-NDefines_CareerProfile.NCareerProfile.MOD_STATISTICS_GROUP_NAME = "BHVM"
+NDefines.NDiplomacy.VOLUNTEERS_PER_TARGET_PROVINCE = 0.5
+NDefines.NDiplomacy.VOLUNTEERS_PER_COUNTRY_ARMY = 0.5
+NDefines.NDiplomacy.VOLUNTEERS_DIVISIONS_REQUIRED = 1
+NDefines.NDiplomacy.TENSION_FACTION_JOIN = 0.1
+NDefines.NDiplomacy.CAPITAL_CAPITULATE_BONUS_SCORE = 10000			-- vanilla 150, changed in attempt to make capitulations look more reasonable. extra bonus when deciding who to capitulate to (applied to capital holder)
+NDefines.NDiplomacy.DIPLOMACY_HOURS_BETWEEN_REQUESTS = 6           -- more spam more spam more spam
+NDefines.NDiplomacy.PEACE_SCORE_PER_PASS = 100.0						-- When you pass once you should get enough points to finish the peace deal
 
+
+NDefines.NCountry.COUNTRY_SCORE_MULTIPLIER = 0				-- Weight of the country score.
+NDefines.NCountry.ARMY_SCORE_MULTIPLIER = 0					-- Based on number of armies.
+NDefines.NCountry.NAVY_SCORE_MULTIPLIER = 0					-- Based on number of navies.
+NDefines.NCountry.AIR_SCORE_MULTIPLIER = 0					-- Based on number of planes (which is typically a lot).
+NDefines.NCountry.INDUSTRY_SCORE_MULTIPLIER = 0				-- Based on number of factories.
+NDefines.NCountry.PROVINCE_SCORE_MULTIPLIER = 0				-- Based on number of controlled provinces.
 NDefines.NCountry.POPULATION_YEARLY_GROWTH_BASE = 0.005825                     -- reduced so there won't be copuis amount of mass mob and the save games to be smaller
 NDefines.NCountry.SPECIAL_FORCES_CAP_MIN = 9999						    -- Unlimited special forces
 NDefines.NCountry.SPECIAL_FORCES_CAP_BASE = 0.00                        -- Unlimited special forces
-NDefines.NDiplomacy.VOLUNTEERS_PER_TARGET_PROVINCE = 0.5			    -- Volunteer shit to prevent 2w spam
-NDefines.NDiplomacy.VOLUNTEERS_PER_COUNTRY_ARMY = 0.5				    -- Volunteer shit to prevent 2w spam
-NDefines.NDiplomacy.VOLUNTEERS_DIVISIONS_REQUIRED = 1				    -- Volunteer shit to prevent 2w spam
-NDefines.NDiplomacy.TENSION_FACTION_JOIN = 0.1
-NDefines.NOperatives.AGENCY_CREATION_FACTORIES = 1				        -- was 5 , but minors having to micro more is good
-NDefines.NPolitics.BASE_POLITICAL_POWER_INCREASE = 2.0 					--fake shit 
 
-NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 9999                            -- WAS 500 || XP Cap
-NDefines.NMilitary.MAX_AIR_EXPERIENCE = 9999			                    -- WAS 500 || XP Cap
-NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 9999		                    -- WAS 500 || XP Cap
-
+NDefines.NProduction.RAILWAY_GUN_MAX_MIL_FACTORIES_PER_LINE = 10
 NDefines.NProduction.LICENSE_IC_COST_YEAR_INCREASE = 0					-- Free license
 NDefines.NProduction.MIN_LICENSE_ACTIVE_DAYS = 1                        -- Free license
 NDefines.NProduction.BASE_LICENSE_IC_COST = 0						    -- Base IC cost for lended license
-NDefines.NProduction.MIN_LAND_EQUIPMENT_CONVERSION_RESOURCE_COST_FACTOR = 1						    -- Metuh
+NDefines.NProduction.MIN_LAND_EQUIPMENT_CONVERSION_RESOURCE_COST_FACTOR = 1
+NDefines.NProduction.MIN_POSSIBLE_TRAINING_MANPOWER = 25000000    -- Increased so most nations don't need to queue up multiple lines of infantry or spam 2w infantry and convert
+NDefines.NProduction.EQUIPMENT_MODULE_ADD_XP_COST = 0				    -- XP cost for adding a new equipment module in an empty slot when creating an equipment variant.
+NDefines.NProduction.EQUIPMENT_MODULE_REPLACE_XP_COST = 0				-- XP cost for replacing one equipment module with an unrelated module when creating an equipment variant.
+NDefines.NProduction.EQUIPMENT_MODULE_CONVERT_XP_COST = 0				-- XP cost for converting one equipment module to a related module when creating an equipment variant.
+NDefines.NProduction.EQUIPMENT_MODULE_REMOVE_XP_COST = 0
 
+NDefines.NTechnology.BASE_RESEARCH_POINTS_SAVED = 40.0 -- base game 30 changed to 40
+
+-- NDefines.NBuildings.OWNER_CHANGE_EXTRA_SHARED_SLOTS_FACTOR = 0.9 -- You get 90% of all factories was 0.5
+
+NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 50
+NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 9999                            -- WAS 500 || XP Cap
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 9999			                    -- WAS 500 || XP Cap
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 9999		                    -- WAS 500 || XP Cap
 NDefines.NMilitary.UNIT_EXPERIENCE_PER_TRAINING_DAY = 0.001 	-- vilkata defines
 NDefines.NMilitary.BASE_LEADER_TRAIT_GAIN_XP = 0.15	-- vilkata defines
 NDefines.NMilitary.FIELD_MARSHAL_XP_RATIO = 0.1 	-- vilkata defines
@@ -31,15 +51,16 @@ NDefines.NMilitary.XP_GAIN_PER_OVERRUN_UNIT = 25	-- vilkata defines
 NDefines.NMilitary.XP_GAIN_FOR_SHATTERING = 25 	-- vilkata defines
 NDefines.NMilitary.UNIT_EXPERIENCE_PER_COMBAT_HOUR = 0.0001
 NDefines.NMilitary.UNIT_EXP_LEVELS = { 0.1, 0.3, 0.75, 0.9 }		-- Experience needed to progress to the next level
-NDefines.NNavy.TRAINING_ACCIDENT_CHANCES = 0   -- vilkata defines
--- NDefines.NNavy.SHORE_BOMBARDMENT_CAP = 0.2 -- vilkata defines, shore cap
 NDefines.NMilitary.BATALION_CHANGED_EXPERIENCE_DROP = 0.05 -- was 0.5
-NDefines.NMilitary.EXPERIENCE_COMBAT_FACTOR = 0.25
-NDefines.NMilitary.ARMOR_VS_AVERAGE = 0.4			                -- how to weight in highest armor & pen vs the division average
-NDefines.NMilitary.PEN_VS_AVERAGE = 0.4
-NDefines.NMilitary.COMBAT_MINIMUM_TIME = 4 --was 4
-
-	NDefines.NAir.MISSION_COMMAND_POWER_COSTS = { -- vilkata defines strongest deversus modding right here
+NDefines.NMilitary.ENCIRCLED_DISBAND_MANPOWER_FACTOR = 0
+NDefines.NMilitary.PROMOTE_LEADER_CP_COST = 0.05 -- was something along the lines of 20
+NDefines.NMilitary.RELIABILTY_RECOVERY = 0.001 -- WAS 0.1 , reason why i keep it is not to loose so much ic, factor affecting how much equipment is returned "from the dead"
+NDefines.NMilitary.DEPLOY_TRAINING_MAX_LEVEL = 2 -- from lvl 1 to lvl 2 divs
+NDefines.NMilitary.UNIT_LEADER_ASSIGN_TRAIT_COST = 0.05
+NDefines.NMilitary.BASE_DIVISION_BRIGADE_GROUP_COST = 0 	--Base cost to unlock a regiment slot,
+NDefines.NMilitary.BASE_DIVISION_BRIGADE_CHANGE_COST = 0	--Base cost to change a regiment column.
+NDefines.NMilitary.BASE_DIVISION_SUPPORT_SLOT_COST = 0 	--Base cost to unlock a support slot
+NDefines.NAir.MISSION_COMMAND_POWER_COSTS = { -- vilkata defines strongest deversus modding right here
 		0.0, -- AIR_SUPERIORITY
 		0.0, -- CAS
 		0.0, -- INTERCEPTION
@@ -56,22 +77,33 @@ NDefines.NMilitary.COMBAT_MINIMUM_TIME = 4 --was 4
 		10.0, -- NAVAL_MINES_SWEEPING
 		0.0, -- MISSION_RECON
 	}
---NDefines.NAir.AIR_MORE_GROUND_CREWS_COST = 10.0					-- CP cost to maintain more ground crews
---NDefines.NAir.AIR_MORE_GROUND_CREWS_BOOST = 0.10					-- Efficienct boost for more ground crews
---NDefines.NAir.COMBAT_MAX_WINGS_AT_ONCE_PORT_STRIKE = 10         -- we can really pounce a naval strike and escalate (10k Vanilla)
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_NO_TRUCK_DISRUPTION_FACTOR = 0.001 -- If a unit isn't motorized, still disrupt its supply by damage * this
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_TRUCK_DAMAGE_FACTOR = 0.008
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_INFRA_DAMAGE_SPILL_FACTOR = 0.001 -- Portion of truck damage to additionally deal to infrastructure
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_TRAIN_DAMAGE_FACTOR = 0.03
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_TRAIN_DAMAGE_DISRUPTION_MITIGATION = 6.0 -- Multiply Train Damage by (Smooth / (Smooth + (Disruption * Mitigation)))
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_TRAIN_DAMAGE_DISRUPTION_SMOOTHING = 2.5
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_RAILWAY_DAMAGE_SPILL_FACTOR = 0.025 -- Portion of train damage to additionally deal to railways
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_DISRUPTION_MIN_DAMAGE_FACTOR = 0.025 -- Multiply train damage by this factor, scale from 1.0 at 0 disruption to this at AIR_WING_ATTACK_LOGISTICS_MAX_DISRUPTION_DAMAGE_TO_CONSIDER
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_MAX_DISRUPTION_DAMAGE_TO_CONSIDER = 5.0 -- see above
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_DIRECT_DISRUPTION_DAMAGE_FACTOR = 0.025 -- Disruption damage to supply throughput done by bombing damage, not dependant on killing trains which also causes diruption.
-NDefines.NAir.AIR_WING_ATTACK_LOGISTICS_TRUCK_MAX_FACTOR = 0.4 -- was 0.3 max trucks we can destroy in one instance of a logistics strike 
---NDefines.NAir.PORT_STRIKES_DELAY_MULTIPLIER = 8                 -- multplies HOURS_DELAY_AFTER_EACH_COMBAT (4 in vanilla, 2 in the mod) if port strikes
--- Anti-Autism
+NDefines.NAir.AIR_WING_FLIGHT_SPEED_MULT = 0.5 --makes redeployement of fighters faster vanilla is 0.02
+NDefines.NAir.AIR_DEPLOYMENT_DAYS = 0                              -- Down from 3 | Makes AC player have brain 
+
+
+NDefines.NNavy.TRAINING_ACCIDENT_CHANCES = 0   -- vilkata defines
+
+
+NDefines.NTrade.ANTI_MONOPOLY_TRADE_FACTOR = 0					-- WAS -100 this group reduces the number of opinion/trade factor changes the game tracks| This is added to the factor value when anti-monopoly threshold is exceeded; cucks majors often if the value is vanilla
+NDefines.NTrade.PARTY_SUPPORT_TRADE_FACTOR = 0			-- Trade factor bonus at the other side having 100 % party popularity for my party
+NDefines.NTrade.ANTI_MONOPOLY_TRADE_FACTOR_THRESHOLD = 0	-- What percentage of resources has to be sold to the buyer for the anti-monopoly factor to take effect
+NDefines.NTrade.MAX_MONTH_TRADE_FACTOR = 0				-- This is the maximum bonus that can be gained from time
+NDefines.NTrade.DISTANCE_TRADE_FACTOR = 0				-- Trade factor is modified by distance times this
+NDefines.NTrade.RELATION_TRADE_FACTOR = 0				-- Trade factor is modified by Opinion value times this
+
+
+
+
+
+NDefines.NAI.DIPLOMACY_ACCEPT_ATTACHE_BASE = 100
+NDefines.NAI.DIPLOMACY_ACCEPT_ATTACHE_OPINION_TRASHHOLD = 0
+NDefines.NAI.DIPLOMACY_ACCEPT_ATTACHE_OPINION_PENALTY = 0
+NDefines.NAI.GIVE_STATE_CONTROL_MIN_CONTROLLED = 0
+NDefines.NAI.GIVE_STATE_CONTROL_MIN_CONTROL_DIFF = 0
+
+NDefines.NFocus.MAX_SAVED_FOCUS_PROGRESS = 30 
+
+NDefines.NOperatives.AGENCY_CREATION_FACTORIES = 1
 NDefines.NOperatives.BOOST_IDEOLOGY_NATIONAL_COVERAGE_FACTOR = 0
 NDefines.NOperatives.BOOST_IDEOLOGY_MAX_DRIFT_BY_OPERATIVE = 0
 NDefines.NOperatives.BOOST_IDEOLOGY_DRIFT_STACKING_FACTOR = 0
@@ -92,33 +124,122 @@ NDefines.NOperatives.DIPLOMATIC_PRESSURE_MAX_AI_ACCEPTANCE_SCORE_INCREASE = 0
 NDefines.NOperatives.DIPLOMATIC_PRESSURE_MAX_TENSION_REQUIREMENTS_DECREASE = 0
 NDefines.NOperatives.DIPLOMATIC_PRESSURE_DAILY_XP_GAIN = 0
 
---THANKS THRASHY <= whos that lul this is stolen from hvm
-NDefines.NAir.ACE_WING_SIZE_MAX_BONUS = 1                       -- biggest bonus we can get from having a small wing with an ace on
-NDefines.NNavy.INITIAL_ALLOWED_DOCKYARD_RATIO_FOR_REPAIRS = 1.0				-- initially countries will allocate this ratio of dockyards for repairs
-NDefines.NNavy.RESOURCE_EXPORT_PRIORITY = 3 --swapped prio so imports go first
-NDefines.NCountry.COUNTRY_SCORE_MULTIPLIER = 0				-- Weight of the country score.
-NDefines.NCountry.ARMY_SCORE_MULTIPLIER = 0					-- Based on number of armies.
-NDefines.NCountry.NAVY_SCORE_MULTIPLIER = 0					-- Based on number of navies.
-NDefines.NCountry.AIR_SCORE_MULTIPLIER = 0					-- Based on number of planes (which is typically a lot).
-NDefines.NCountry.INDUSTRY_SCORE_MULTIPLIER = 0				-- Based on number of factories.
-NDefines.NCountry.PROVINCE_SCORE_MULTIPLIER = 0				-- Based on number of controlled provinces.
---NDefines.NProduction.ANNEX_FIELD_EQUIPMENT_RATIO = 1	-- WAS 0.25 | Annex decisions should give all troops, but incase I screwed up here is this -Thrasymachus | How much equipment from deployed divisions will be transferred on annexation
-NDefines.NProduction.MIN_POSSIBLE_TRAINING_MANPOWER = 25000000    -- Increased so most nations don't need to queue up multiple lines of infantry or spam 2w infantry and convert
 NDefines.NBuildings.MAX_SHARED_SLOTS = 99 -- WAS 25 | Increased to accommodate Germany/Japan building slot changes in order to support more of their eco being in their cores.
-NDefines.NDiplomacy.CAPITAL_CAPITULATE_BONUS_SCORE = 10000			-- vanilla 150, changed in attempt to make capitulations look more reasonable. extra bonus when deciding who to capitulate to (applied to capital holder)
-NDefines.NDiplomacy.DIPLOMACY_HOURS_BETWEEN_REQUESTS = 6           -- more spam more spam more spam
-NDefines.NDiplomacy.PEACE_SCORE_PER_PASS = 100.0						-- When you pass once you should get enough points to finish the peace deal
-NDefines.NAir.AIR_WING_FLIGHT_SPEED_MULT = 0.5 --makes redeployement of fighters faster vanilla is 0.02
-NDefines.NAir.AIR_DEPLOYMENT_DAYS = 0                              -- Down from 3 | Makes AC player have brain 
-NDefines.NMilitary.ENCIRCLED_DISBAND_MANPOWER_FACTOR = 0            -- WAS 0.2 | Most rulesets ban deleting encircled troops, but at least this prevents some manpower from returning | Deleting encircled divisions is always banned anyways, so this reduces unfair play | percentage of manpower returned when an encircled unit is disbanded
-NDefines.NTrade.ANTI_MONOPOLY_TRADE_FACTOR = 0					-- WAS -100 this group reduces the number of opinion/trade factor changes the game tracks| This is added to the factor value when anti-monopoly threshold is exceeded; cucks majors often if the value is vanilla
-NDefines.NTrade.PARTY_SUPPORT_TRADE_FACTOR = 0			-- Trade factor bonus at the other side having 100 % party popularity for my party
-NDefines.NTrade.ANTI_MONOPOLY_TRADE_FACTOR_THRESHOLD = 0	-- What percentage of resources has to be sold to the buyer for the anti-monopoly factor to take effect
-NDefines.NTrade.MAX_MONTH_TRADE_FACTOR = 0				-- This is the maximum bonus that can be gained from time
-NDefines.NTrade.DISTANCE_TRADE_FACTOR = 0				-- Trade factor is modified by distance times this
-NDefines.NTrade.RELATION_TRADE_FACTOR = 0				-- Trade factor is modified by Opinion value times this
-NDefines.NBuildings.OWNER_CHANGE_EXTRA_SHARED_SLOTS_FACTOR = 0.9 -- You get 90% of all factories was 0.5
-NDefines.NBuildings.SUPPLY_ROUTE_RESOURCE_BONUS = 0.3 -- was 0.2 but it felt funny to make it 0.3 uwu
 
--------------------------------------------------------------------------------------------
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+NDefines_Graphics.NGraphics.MAX_MESHES_LOADED_PER_FRAME = 5
+NDefines_Graphics.NMapMode.RADAR_ROTATION_SPEED = 0.0
+NDefines_Graphics.NMapMode.AIR_RANGE_INDICATOR_ROTATION_SPEED = 0.0000
+
+NDefines_Graphics.NGraphics.MIN_TRAIN_WAGON_COUNT = 1
+NDefines_Graphics.NGraphics.MAX_TRAIN_WAGON_COUNT = 1
+
+NDefines.NInterface.MINIMAP_PING_DELAY_BETWEEN_PINGS = 0.5 --vanilla 0.30 -- delay between consecative pings
+
+NDefines_Graphics.NMapMode.MAP_MODE_TERRAIN_TRANSPARENCY = 0.8
+
+NDefines_Graphics.NGraphics.VICTORY_POINTS_DISTANCE_CUTOFF = {250, 500, 1000}
+NDefines_Graphics.NGraphics.MAP_ICONS_GROUP_CAM_DISTANCE = 90.0 -- camera distance at which the icons begin to group up
+NDefines_Graphics.NGraphics.MAP_ICONS_STATE_GROUP_CAM_DISTANCE = 350.0 -- Camera distance at which the icons begin to group up on state level
+NDefines_Graphics.NGraphics.MAP_ICONS_STRATEGIC_GROUP_CAM_DISTANCE = 350 -- second camera distance at which the icons begin to group up
+NDefines_Graphics.NGraphics.UNITS_DISTANCE_CUTOFF = 200
+
+NDefines_Graphics.NGraphics.SUPPLY_ICON_UNUSED_CUTOFF = 900.0        -- where we stop showing unused nodes
+NDefines_Graphics.NGraphics.SUPPLY_ICON_NUMBERS_CUTOFF = 900.0       -- where we stop showing numbers on hubs (ignored for selected and problem hubs)
+
+NDefines_Graphics.NGraphics.DRAW_FOW_CUTOFF = 0
+NDefines_Graphics.NGraphics.DRAW_FOW_FADE_LENGTH = 0
+NDefines_Graphics.NGraphics.DRAW_SHADOWS_CUTOFF = 0
+NDefines_Graphics.NGraphics.DRAW_SHADOWS_FADE_LENGTH = 0
+NDefines_Graphics.NGraphics.TREE_FADE_NEAR = 150.0
+NDefines_Graphics.NGraphics.TREE_FADE_FAR = 250.0
+NDefines_Graphics.NGraphics.BLOOM_WIDTH = 0
+NDefines_Graphics.NGraphics.BLOOM_SCALE = 0
+NDefines_Graphics.NGraphics.BRIGHT_THRESHOLD = 0
+NDefines_Graphics.NGraphics.EMISSIVE_BLOOM_STRENGTH = 0
+
+NDefines_Graphics.NGraphics.UNITS_ICONS_DISTANCE_CUTOFF = 900
+NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {250, 500, 1000}
+NDefines_Graphics.NGraphics.SUPPLY_ICON_CUTOFF = 900.0               -- total supply icon cutoff distance for all
+NDefines_Graphics.NGraphics.SUPPLY_ICON_UNUSED_CUTOFF = 900.0        -- where we stop showing unused nodes
+NDefines_Graphics.NGraphics.SUPPLY_ICON_NUMBERS_CUTOFF = 900.0       -- where we stop showing numbers on hubs (ignored for selected and problem hubs)
+NDefines_Graphics.NGraphics.RESOURCE_MAP_ICON_TEXT_CUTOFF = 1000  -- At what camera distance the resource name/amount text disappears.
+
+NDefines_Graphics.NGraphics.RAILWAY_CAMERA_CUTOFF = 300.0 -- railways are cut off above this camera height
+NDefines_Graphics.NGraphics.RAILWAY_CAMERA_CUTOFF_SPEED = 4.0 -- railways fade in/out speed
+
+
+
+
+NDefines_Graphics.NGraphics.CAMERA_ZOOM_SPEED_DISTANCE_MULT = 20
+NDefines_Graphics.NGraphics.MAPICON_GROUP_STRATEGIC_SIZE = 1000
+NDefines_Graphics.NGraphics.MAP_ICONS_STATE_HUGE = 100
+NDefines_Graphics.NGraphics.PROVINCE_ANIM_TEXT_DISTANCE_CUTOFF = 200
+NDefines_Graphics.NGraphics.AIRBASE_ICON_DISTANCE_CUTOFF = 600
+NDefines_Graphics.NGraphics.NAVALBASE_ICON_DISTANCE_CUTOFF = 600
+NDefines_Graphics.NGraphics.RADAR_ICON_DISTANCE_CUTOFF = 150
+-- NDefines_Graphics.NGraphics.DRAW_MAP_OBJECTS_CUTOFF = 1.0
+NDefines_Graphics.NInterface.GRIDBOX_ELEMENTS_INTERPOLATION_SPEED = 0.2
+NDefines_Graphics.NGraphics.UNIT_ARROW_DISTANCE_CUTOFF = 500
+NDefines_Graphics.NGraphics.NAVAL_COMBAT_DISTANCE_CUTOFF = 1000
+NDefines_Graphics.NGraphics.ADJACENCY_RULE_DISTANCE_CUTOFF = 1300
+NDefines_Graphics.NGraphics.LAND_COMBAT_DISTANCE_CUTOFF = 800
+NDefines_Graphics.NGraphics.SUPPLY_ICON_DISTANCE_CUTOFF = 1500
+NDefines_Graphics.NGraphics.PROV_CONSTRUCTION_ICON_DISTANCE_CUTOFF = 300
+NDefines_Graphics.NGraphics.STATE_CONSTRUCTION_ICON_DISTANCE_CUTOFF = 600
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_REFRESH_FREQ = 0.2
+NDefines_Graphics.NGraphics.MAPICON_GROUP_PASSES = 5
+-- NDefines_Graphics.NGraphics.DRAW_REFRACTIONS_CUTOFF = 0
+NDefines_Graphics.NGraphics.GRADIENT_BORDERS_FIELD_COUNTRY_REFRESH = 40
+NDefines_Graphics.NGraphics.WEATHER_DISTANCE_CUTOFF = 50
+NDefines_Graphics.NGraphics.MAP_ICONS_STRATEGIC_AREA_HUGE = 250
+NDefines_Graphics.NGraphics.MAP_ICONS_COARSE_COUNTRY_GROUPING_DISTANCE = 300
+NDefines_Graphics.NGraphics.MAP_ICONS_COARSE_COUNTRY_GROUPING_DISTANCE_STRATEGIC = 0
+NDefines_Graphics.NGraphics.DAY_NIGHT_FEATHER = 0.024

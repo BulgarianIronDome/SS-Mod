c02731f9c60ebe4d6868e86ce079d53d68bae5f2
DeversusOrg <101480105+DeversusOrg@users.noreply.github.com>
2024-11-27 07:18:21 +0200

    Optimization vol 1
    The game for sure feels slower than before , will have to figure out why and how

--- common/defines/BHVM.lua
+++ common/defines/BHVM.lua
@@ -1,6 +1,6 @@
 NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 999 -- Lag days
 NDefines.NGame.LAG_DAYS_FOR_PAUSE = 999 -- pause days
-NDefines.NGame.GAME_SPEED_SECONDS = { 6000.0, 0.25, 0.19, 0.06, 0.0 }
+NDefines.NGame.GAME_SPEED_SECONDS = { 6000.0, 0.75, 0.22, 0.08, 0.0 }
 NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 14
 NDefines.NGame.MESSAGE_TIMEOUT_DAYS = 14
 
